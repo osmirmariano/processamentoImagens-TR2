79e0c2f46b0354651a233da094756f10e54f223d
osmirmariano <osmirmariano@uft.edu.br>
2017-03-05 19:20:18 -0300

    Criando o projeto e a implementação de transformar a imagem em binária já realizada, agora falta implementar somente o algoritmo de rotulação

---
+++ rotulacao.h
@@ -0,0 +1,29 @@
+//---------------------------------------------------------------------------
+
+#ifndef rotulacaoH
+#define rotulacaoH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.Buttons.hpp>
+#include <Vcl.ExtCtrls.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *Image1;
+	TSpeedButton *SpeedButton1;
+	TSpeedButton *SpeedButton2;
+	void __fastcall SpeedButton1Click(TObject *Sender);
+	void __fastcall SpeedButton2Click(TObject *Sender);
+private:	// User declarations
+	TColor cor;
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
